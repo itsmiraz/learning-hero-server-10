1ba2c964e0ce537558f022a3f0c8b3dc58a34170
itsmiraz <mirajulislamofficial044@gmail.com>
2022-10-25 09:19:33 +0600

    setting catagory data

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 5000
+var cors = require('cors')
 
+app.use(cors())
+// const courses = require('/data/courses.json')
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Server is running on ${port}`)
 })
